93ec75b169181f87d4cd881e4c959885e5deb527
hwx0016 <hwx0016@aliyun.com>
2018-08-23 23:47:23 +0800

    js wait for minites

---
+++ node_wait.js
@@ -0,0 +1,12 @@
+/*js实现sleep功能 单位：毫秒*/
+function sleep(numberMillis) {
+    var now = new Date();
+    var exitTime = now.getTime() + numberMillis;
+    while (true) {
+        now = new Date();
+        if (now.getTime() > exitTime)
+            return;
+    }
+}
+
+setTimeOut(f(),ms);
